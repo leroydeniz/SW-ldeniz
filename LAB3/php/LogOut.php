c720a6480a1e360380a62c4142918ce16f72ca6a
leroydeniz <61316853+leroydeniz@users.noreply.github.com>
2020-11-07 17:47:39 +0100

    Add files via upload

---
+++ LAB3/php/LogOut.php
@@ -0,0 +1,16 @@
+<?php
+    if(isset($_GET["email"])) { 
+        $email = $_GET["email"];
+        echo "<script>alert(\"¡Adiós $email!\");document.location.href='Layout.php';</script>"; 
+    } else {
+        echo "<script>document.location.href='Layout.php';</script>"; 
+    }
+?>
+
+
+
+
+
+
+
+
---
+++ LAB3/php/Menus.php
@@ -0,0 +1,11 @@
+<div id='page-wrap'>
+<header class='main' id='h1'>
+    <span class="right menusup">Anónimo <img style='max-width:120px; max-height:60px;' alt='anonimo' src='../images/anonymus.jpg'/></span><br/><br/>
+    <span class="right menusup"><a href="SignUp.php">Registro</a></span>
+    <span class="right menusup"><a href="LogIn.php">Login</a></span>
+
+</header>
+<nav class='main' id='n1' role='navigation'>
+  <span><a href='Layout.php'>Inicio</a></span>
+  <span><a href='Credits.php'>Creditos</a></span>
+</nav>
