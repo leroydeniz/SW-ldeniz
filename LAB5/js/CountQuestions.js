d1f1afc1f9a019d47caf503e676dd7351e8124da
leroydeniz <61316853+leroydeniz@users.noreply.github.com>
2020-11-15 00:24:31 +0100

    Add files via upload

--- LAB5/js/CountQuestions.js
+++ LAB5/js/CountQuestions.js
@@ -3,7 +3,7 @@ setInterval(function() {
         /* creamos la variable con la solicitud */
         xhr = new XMLHttpRequest();
         
-        /* indico qué traer y le paso el parámetro de email para saber las preguntas del usuaropio */
+        /* indico qué traer y le paso el parámetro de email para saber las preguntas del usuario */
         xhr.open('GET','../php/CountXMLQuestions.php?email='+$("#email").val(), true);
         
         /* le digo qué hacer cuando llegue la respuesta */
@@ -14,4 +14,20 @@ setInterval(function() {
             }
         }
         xhr.send('');
-}, 10000);
+        
+        /* creamos la variable con la solicitud */
+        xhr2 = new XMLHttpRequest();
+        
+        /* indico qué traer y le paso el parámetro de email para saber las preguntas del usuario */
+        xhr2.open('GET','../php/CountXMLUsers.php', true);
+        
+        /* le digo qué hacer cuando llegue la respuesta */
+        xhr2.onreadystatechange = function(){
+            if(xhr.readyState == 4 && xhr2.status == 200) {
+                //$("#ucounter").html(xhr2.responseText);
+                document.getElementById('ucounter').innerHTML = xhr2.responseText;
+            }
+        }
+        xhr2.send('');
+}, 2000);
+
